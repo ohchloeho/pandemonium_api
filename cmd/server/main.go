e8d268a052b6e1c88e0619860b5d2569ae44572c
Chloe Ho <chloeho@Chloes-Mac-mini.lan>
2024-11-09 23:17:10 +0800

    [TS] - Update error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"pandemonium_api/api"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -14,8 +16,18 @@ func main() {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to connect to MongoDB:", err)
 	}
+
+	// Ping the MongoDB server to ensure the connection is successful
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB")
 
 	// Connect to a specific database
 	db := client.Database("pandaDB")
