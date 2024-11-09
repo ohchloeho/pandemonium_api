2a3c1278fcff9e1d9ee130fbb3641f26c9e03a18
Chloe Ho <chloeho@Chloes-Mac-mini.lan>
2024-11-09 21:25:37 +0800

    [TS] - Router Endpoint Error

--- api/handlers/project.go
+++ api/handlers/project.go
@@ -1,31 +1,32 @@
 package handlers
 
 import (
-    "net/http"
+	"net/http"
+	"pandemonium_api/internal/database/models"
 	"pandemonium_api/internal/services"
-    "pandemonium_api/internal/database/models"
-    "github.com/gin-gonic/gin"
+
+	"github.com/gin-gonic/gin"
 )
 
 type ProjectHandler struct {
-    projectService *services.ProjectService
+	projectService *services.ProjectService
 }
 
 func NewProjectHandler(service *services.ProjectService) *ProjectHandler {
-    return &ProjectHandler{projectService: service}
+	return &ProjectHandler{projectService: service}
 }
 
 func (h *ProjectHandler) CreateProject(c *gin.Context) {
-    var project models.Project
-    if err := c.ShouldBindJSON(&project); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    projectData := services.ProjectData{
-        Name:        project.Name,
-        Description: project.Description,
-        CreatedBy:   project.CreatedBy,
-    }
-    h.projectService.CreateProject(projectData)
-    c.JSON(http.StatusCreated, project)
+	var project models.Project
+	if err := c.ShouldBindJSON(&project); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	projectData := services.ProjectData{
+		Name:        project.Name,
+		Description: project.Description,
+		CreatedBy:   project.CreatedBy,
+	}
+	h.projectService.CreateProject(projectData)
+	c.JSON(200, gin.H{"status": "project created"})
 }
