2a4fb0ac86b0f404fc62ac3c5e08ea21861f0df5
Chloe Ho <chloeho@Chloes-Mac-mini.lan>
2024-11-10 15:32:55 +0800

    [TS] - Test router setup & debug

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,56 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"net/http"
+	"pandemonium_api/api"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// MongoDB client setup
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
 
-	// Create a new Gin router
-	router := gin.Default()
+	// Ping the MongoDB server to ensure the connection is successful
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
+	}
 
-	// POST route definition
-	router.POST("/test", func(c *gin.Context) {
-		var json map[string]interface{}
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"status": "success", "data": json})
-	})
+	fmt.Println("Successfully connected to MongoDB")
 
-	// Run the server
+	// Connect to a specific database
+	db := client.Database("pandaDB")
+
+	// Initialize the Gin router
+	router := api.SetupRouter(db)
+
+	// Start the server
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal("Unable to start server: ", err)
 	}
-	// // MongoDB client setup
-	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	// client, err := mongo.Connect(context.Background(), clientOptions)
-	// if err != nil {
-	// 	log.Fatal("Failed to connect to MongoDB:", err)
-	// }
-
-	// // Ping the MongoDB server to ensure the connection is successful
-	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// defer cancel()
-	// err = client.Ping(ctx, nil)
-	// if err != nil {
-	// 	log.Fatal("Failed to ping MongoDB:", err)
-	// }
-
-	// fmt.Println("Successfully connected to MongoDB")
-
-	// // Connect to a specific database
-	// db := client.Database("pandaDB")
-
-	// // Initialize the Gin router
-	// router := api.SetupRouter(db)
-
-	// // Start the server
-	// if err := router.Run(":8080"); err != nil {
-	// 	log.Fatal("Unable to start server: ", err)
-	// }
 }
