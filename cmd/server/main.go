3c31e3f6f9db60a13a4a0043d34f53118fef4fc2
Chloe Ho <chloeho@Chloes-Mac-mini.lan>
2024-11-10 16:21:11 +0800

    [CleanUP] - Update db.go and main.go for cleaner codebase

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,39 +1,25 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"pandemonium_api/api"
-	"time"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"pandemonium_api/internal/database"
 )
 
-func main() {
-	// MongoDB client setup
-	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
-	client, err := mongo.Connect(context.Background(), clientOptions)
+func initDB() *database.DB {
+	db, err := database.NewDB()
 	if err != nil {
 		log.Fatal("Failed to connect to MongoDB:", err)
 	}
+	return db
+}
 
-	// Ping the MongoDB server to ensure the connection is successful
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal("Failed to ping MongoDB:", err)
-	}
-
-	fmt.Println("Successfully connected to MongoDB")
-
-	// Connect to a specific database
-	db := client.Database("pandaDB")
+func main() {
+	db := initDB()
+	defer db.Close()
 
 	// Initialize the Gin router
-	router := api.SetupRouter(db)
+	router := api.SetupRouter(db.Database)
 
 	// Start the server
 	if err := router.Run(":8080"); err != nil {
